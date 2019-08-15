1b531a0b125a47c2c8b87954744c718c29e81796
Sidhant Gandhi <>
2019-08-15 12:53:32 -0700

    Flexbox for time slots

---
+++ src/components/Flexbox.svelte
@@ -0,0 +1,47 @@
+<script>
+    import css from 'svelte-css-vars';
+
+    export let direction = undefined;
+    export let wrap = undefined;
+    export let order = undefined;
+    export let grow = undefined;
+    export let shrink = undefined;
+    export let basis = undefined;
+    export let justifyContent = undefined;
+    export let alignSelf = undefined;
+    export let alignItems = undefined;
+    export let alignContent = undefined;
+
+    $: style = {
+        direction,
+        justifyContent,
+        wrap,
+        order,
+        grow,
+        shrink,
+        basis,
+        alignSelf,
+        alignItems,
+        alignContent,
+    };
+</script>
+
+<style>
+    .flexbox {
+        display: flex;
+        flex-direction: var(--direction, row);
+        flex-wrap: var(--wrap, nowrap);
+        order: var(--order, 0);
+        flex-grow: var(--grow, 0);
+        flex-shrink: var(--shrink, 1);
+        flex-basis: var(--basis, auto);
+        justify-content: var(--justifyContent, flex-start);
+        align-self: var(--alignSelf, auto);
+        align-items: var(--alignItems, stretch);
+        align-content: var(--alignContent, stretch);
+    }
+</style>
+
+<div use:css={style} class='flexbox'>
+    <slot />
+</div>
--- src/routes/Slots.svelte
+++ src/routes/Slots.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import { navigate } from 'svelte-routing';
     import moment from 'moment-timezone';
+    import Flexbox from '../components/Flexbox.svelte';
     import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from '../../lib/authConstants'
     export let slots;
 
@@ -21,18 +22,32 @@
     });
 </script>
 
+<style>
+    .slot-container {
+        background: lavender;
+    }
+    .slot {
+        background: coral;
+        padding: 1rem;
+    }
+</style>
+
 <main>
     <h1>Open time slots</h1>
-    <h2>
-        These are the times you are free
-    </h2>
+    <h2>These are the times you are free</h2>
     <p>
     {#if slots}
-        <ul>
-        {#each slots as slot}
-            <li>{moment(slot.start).format('LLL')} - {moment(slot.end).format('LLL')}</li>
-        {/each}
-        </ul>
+        <div class='slot-container' width='100%'>
+            <Flexbox wrap={'wrap'} justifyContent={'space-evenly'}>
+            {#each slots as slot}
+                <Flexbox>
+                    <div class='slot'>
+                        {moment(slot.start).format('l LT')} - {moment(slot.end).format('l LT')}
+                    </div>
+                </Flexbox>
+            {/each}
+            </Flexbox>
+        </div>
     {:else}
         Loading...
     {/if}
